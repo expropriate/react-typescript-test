58089c70931907a46dcf6537937abd45917fc623
expropriate <105367096+expropriate@users.noreply.github.com>
2023-03-07 17:42:07 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { Practice1 } from "./pactices/Practice1";
+import { Practice2 } from "./pactices/Practice2";
+import { Practice3 } from "./pactices/Practice3";
+import { Practice4 } from "./pactices/Practice4";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
+    </div>
+  );
+}
---
+++ src/typeList.ts
@@ -0,0 +1,30 @@
+/**　基本の型 */
+let bool: boolean = true;
+
+let num: number = 0;
+
+let str: String = "CCC";
+
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+// tuple
+let tuple: [number, string] = [0, "CCC"];
+
+// any
+let any: any = false;
+
+// vooid
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+// null
+let null1: null = null;
+
+// undefind
+let undefind1: undefind = undefind;
+
+// object
+let obj1: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "ccc" };
